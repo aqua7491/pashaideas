65347e19dec4b31478ad4466a4c4ae439c93817c
aqua7491 <poul.ilchyk@hotmail.com>
2017-03-14 22:16:40 +0200

    Dynamic page

--- pages/views.py
+++ pages/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
-
 from django.views.generic import View, FormView, CreateView
-
 from newsletter.forms import JoinForm
-
+from .models import Page
+from django.contrib.messages.views import SuccessMessageMixin
 # class HomeView(View):
 #     def get(self, request, *args, **kwargs):
 #         return render(request, "pages/home.html", {})
 
 
-class HomeView(CreateView):
+class HomeView(SuccessMessageMixin, CreateView):
     template_name = "pages/home.html"
     form_class = JoinForm
     success_url = '/'
 
+    def get_success_message(self, cleaned_data):
+        return "Thank you for joining!"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['page_obj'] = Page.objects.all().first()
+        return context
     # def form_valid(self, form):
     #     email = form.cleaned_data.get("email")
     #     return super(HomeView, self).form_valid(form)
