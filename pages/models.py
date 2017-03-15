ce8d804b64063392f926aa23cebd1b179d3be3b2
aqua7491 <poul.ilchyk@hotmail.com>
2017-03-15 15:46:49 +0200

    updated layout/formatting

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+LAYOUT_CHOICES = (
+    ('standart', 'Standart'),
+    ('staked', 'Staked'),
+    )
+
 
 class Page(models.Model):
     title = models.CharField(max_length=220)
@@ -7,6 +12,10 @@ class Page(models.Model):
     title_btn = models.CharField(max_length=50, default='Join')
     title_btn_url = models.CharField(max_length=50, blank=50, null=True)
     content = models.TextField(blank=True, null=True)
+    show_nav = models.BooleanField(default=True)
+    nav_color = models.CharField(max_length=7, default='#000000')
+    layout = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default='standart')
+    video_embed = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
