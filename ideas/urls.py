d19a33098bc42d1dc2c8fcee46c930fd01ceff40
aqua7491 <poul.ilchyk@hotmail.com>
2017-03-16 18:32:22 +0200

    Added AJAX for Form

--- ideas/urls.py
+++ ideas/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from pages.views import HomeView
+from newsletter.api.views import JoinCreateAPIView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view(), name='home')
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^api/email/join/$', JoinCreateAPIView.as_view(), name='email-join')
 ]
