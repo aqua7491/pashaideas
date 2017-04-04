e3de7d787e2d24f5ee0d46ff12510ae03764faf3
aqua7491 <poul.ilchyk@hotmail.com>
2017-04-04 20:48:54 +0300

    Added slugfield

--- ideas/urls.py
+++ ideas/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from pages.views import HomeView
+from pages.views import HomeView, PageDetailView
 from newsletter.api.views import JoinCreateAPIView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^(P<slug>[\w-]+)$', PageDetailView.as_view(), name='page-detail'),
     url(r'^api/email/join/$', JoinCreateAPIView.as_view(), name='email-join')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, FormView, CreateView
+from django.views.generic import View, FormView, CreateView, DetailView
 from newsletter.forms import JoinForm
 from .models import Page
 from django.contrib.messages.views import SuccessMessageMixin
@@ -18,8 +18,14 @@ class HomeView(SuccessMessageMixin, CreateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
-        context['page_obj'] = Page.objects.all().first()
+        context['page_obj'] = Page.objects.filter(featured=True).first()
         return context
     # def form_valid(self, form):
     #     email = form.cleaned_data.get("email")
     #     return super(HomeView, self).form_valid(form)
+
+
+class PageDetailView(DetailView):
+    queryset = Page.objects.filter(active=True)
+    model = Page
+    
